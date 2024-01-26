cfbb21e6ff48d320ef3939475e3d1b0bd4c6a438
jukkyni <87795531+jukkyni@users.noreply.github.com>
2024-01-26 10:02:24 +0900

    タイトル画面を改良しました
    ユーザ名を導入して、やサーバ設定をコードの書き換えをせずにタイトルで行えるようにしました。

--- TETRIS.py
+++ TETRIS.py
@@ -1,19 +1,18 @@
 #################### Rough TETRIS ####################
 import requests
+import socket
 import json
 import tkinter as tk
 import random
 from tkinter import messagebox
-import simpleaudio as sa #simpleaudioのインストールが必要
-import threading
 
-#################### サーバーを指定する ####################
-# サーバーのIPアドレスをip_addressに入力してください
-ip_address = "10.1.53.45"
-# サーバーのポート番号をport_numberに入力してください。
-port_number = "5001"
+#################### サーバを指定する ####################
+# サーバのIPアドレスをip_addressに入力してください
+ip_address = ""
+# サーバのポート番号をport_numberに入力してください。
+port_number = 5000
 
-global play_obj  # グローバル変数として宣言
+username = "名無しさん" # ユーザ名を入力
 
 SIZE = 30       #ブロックのサイズ
 moveX = 4       #テトロミノ表示位置（横）
@@ -45,17 +44,6 @@ for y in range(22):
             sub.append(7)
     field.append(sub)
 
-#音楽再生
-def play_music(file_path):
-    global play_obj
-    wave_obj = sa.WaveObject.from_wave_file(file_path)
-    play_obj = wave_obj.play()
-
-#音楽ストップ
-def music_stop():
-    global play_obj
-    if play_obj.is_playing():
-        play_obj.stop()
 #テトロミノを表示する関数
 def drawTetris():
     for i in range(4):
@@ -71,7 +59,7 @@ def drawField():
             can.create_rectangle(j*SIZE, i*SIZE, (j+1)*SIZE, (i+1)*SIZE, fill=color[field[i+1][j]], width=outLine)
 
 #テトロミノを動かす関数
-def keyPress(event):   
+def keyPress(event):
     global moveX, moveY
     afterX = moveX
     afterY = moveY
@@ -134,46 +122,67 @@ def deleteLine():
             score += 800-timer
     for i in range(1, 11):
         if 7 != field[1][i]:
-
-            music_stop()  # 音楽を停止する
             ## スコア送信
             server_url = "http://"+ip_address+":"+port_number+"/receive_data"
-            data_to_send = {"得点": score}
+            data_to_send = {"ユーザ名":username,"得点": score}
             headers = {'Content-Type': 'application/json'}
             response = requests.post(server_url, data=json.dumps(data_to_send), headers=headers)
-
             messagebox.showinfo("information", "GAME OVER !")
             exit()
 
-####################  ゲームループ  ####################    
+####################  ゲームループ  ####################
 def on_button_click():
-    print("ボタンがクリックされました")
+    global ip_address, port_number,username
+    # タイトル画面で入力した内容を反映させる
+    username = field_username.get()
+    ip_address = field_IP.get()
+    port_number = field_port.get()
     root.destroy()
 
-
-# Tkinterウィンドウの作成
+# 初期設定兼タイトル画面
 root = tk.Tk()
-root.title("Tkinterのボタン例")
+root.geometry("340x630+300+100")
+root.title("レッツプレイ")
 # タイトルバーを非表示にする
-root.overrideredirect(True)
+# root.overrideredirect(True)
 
-# ボタンの作成
-button = tk.Button(root, text="クリックしてください", command=on_button_click)
+# フォームの作成
+form_frame = tk.Frame(root)
+label_user_config = tk.Label(root,text="ユーザ名（ペンネーム、ニックネーム）を入力してください。",anchor="w",justify="left",padx=25,pady=15,wraplength=280)
+label_username = tk.Label(form_frame,text="ユーザ名：")
+field_username = tk.Entry(form_frame,width=20)
+label_user_config.pack()
+label_username.grid(row=0,column=0)
+field_username.grid(row=0,column=1)
+form_frame.pack()
 
-
-# 音楽ファイルのパス
-music_file = 'tetris-theme-korobeiniki-arranged-for-piano-186249.wav'
-
-# 音楽再生用のスレッドを作成し、開始する
-music_thread = threading.Thread(target=play_music, args=(music_file,))
-music_thread.start()
-
-# ボタンの配置
+form_frame_server = tk.Frame(root)
+label_server_config = tk.Label(root,text="接続するサーバのIPアドレスとポート番号を入力してください。",anchor="w",justify="left",padx=25,pady=15,wraplength=280)
+label_IP = tk.Label(form_frame_server,text="IPアドレス：")
+label_port = tk.Label(form_frame_server,text="ポート番号：")
+field_IP = tk.Entry(form_frame_server,width=20)
+field_port = tk.Entry(form_frame_server,width=20)
+label_server_config.pack()
+label_IP.grid(row=0,column=0)
+field_IP.grid(row=0,column=1)
+label_port.grid(row=1,column=0)
+field_port.grid(row=1,column=1)
+form_frame_server.pack()
+button = tk.Button(root, text="ゲーム開始", command=on_button_click)
 button.pack()
 root.mainloop()
 
+# サーバに接続できるか確認します
+try:
+    # print((ip_address,int(port_number)))
+    socket.create_connection((ip_address,int(port_number)),timeout=3)
+except socket.error as e:
+    # サーバに接続できなければ終了します
+    messagebox.showinfo("information", "サーバに接続できませんでした。プログラムを再起動して設定し直してください。")
+    exit()
+
 win = tk.Tk()
-win.geometry("340x630")
+win.geometry("340x630+300+100")
 win.title("TETRIS")
 can = tk.Canvas(win, width=12*SIZE, height=21*SIZE)
 can.place(x=-10, y=0)
